13a7559003814e1384bd24f717c2c02ececbf1b7
Brian Neradt <brian.neradt@gmail.com>
2025-07-01 23:13:42 -0500

    starting a model

--- mysite/workoutlog/models.py
+++ mysite/workoutlog/models.py
@@ -1,3 +1,27 @@
-from django.db import models
+from django.db import models  # type: ignore
+from django.conf import settings  # type: ignore
 
 # Create your models here.
+
+class PullupType(models.TextChoices):
+    PULLUPS = 'PU', 'Pullups'
+    CHINUPS = 'CH', 'Chinups'
+    NEUTRAL = 'NE', 'Neutral'
+
+class WorkoutSession(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    date = models.DateField()
+    pullup_type = models.CharField(
+        max_length=2,
+        choices=PullupType.choices,
+        default=PullupType.PULLUPS,
+    )
+    pullup_count = models.PositiveIntegerField(default=0)
+    pushup_count = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        unique_together = ('user', 'date')
+        ordering = ['-date']
+
+    def __str__(self):
+        return f"{self.user.username} - {self.date} (Pullups: {self.pullup_count} {self.get_pullup_type_display()}, Pushups: {self.pushup_count})"
