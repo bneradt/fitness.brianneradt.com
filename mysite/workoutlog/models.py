eda339d4f4e7275e0d13015ba37f6c3c5f90e940
Brian Neradt <brian.neradt@gmail.com>
2025-07-02 22:34:13 -0500

    This seems to be a decent model

--- mysite/workoutlog/models.py
+++ mysite/workoutlog/models.py
@@ -10,18 +10,33 @@ class PullupType(models.TextChoices):
 
 class WorkoutSession(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    date = models.DateField()
+    date = models.DateField(unique=True)
     pullup_type = models.CharField(
         max_length=2,
         choices=PullupType.choices,
         default=PullupType.PULLUPS,
     )
-    pullup_count = models.PositiveIntegerField(default=0)
-    pushup_count = models.PositiveIntegerField(default=0)
 
     class Meta:
         unique_together = ('user', 'date')
         ordering = ['-date']
 
     def __str__(self):
-        return f"{self.user.username} - {self.date} (Pullups: {self.pullup_count} {self.get_pullup_type_display()}, Pushups: {self.pushup_count})"
+        return f"{self.user.username} - {self.date}"
+
+class WorkoutSet(models.Model):
+    session = models.ForeignKey(WorkoutSession, on_delete=models.CASCADE, related_name='sets')
+    set_number = models.PositiveSmallIntegerField(
+        choices=[(1, 'Set 1'), (2, 'Set 2'), (3, 'Set 3')]
+    )
+    pullup_count = models.PositiveIntegerField(default=0)
+    pushup_count = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        unique_together = ('session', 'set_number')
+        ordering = ['session', 'set_number']
+
+    def __str__(self):
+        user = self.session.user.username
+        date = self.session.date
+        return f"{user} - {date} - Set {self.set_number}"
