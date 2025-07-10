370b823d6c86205cb10b73da664f4ee6710ca215
Brian Neradt <brian.neradt@gmail.com>
2025-07-09 22:04:30 -0400

    placeholder/hint for new fields

--- mysite/workoutlog/forms.py
+++ mysite/workoutlog/forms.py
@@ -25,3 +25,16 @@ class WorkoutSessionForm(forms.ModelForm):
             from django.utils import timezone
             # Use localdate to respect TIME_ZONE and avoid UTC shift
             self.fields['date'].initial = timezone.localdate()
+
+        # Use placeholder for count fields instead of default 0
+        count_fields = [
+            'pullup_count1', 'pushup_count1',
+            'pullup_count2', 'pushup_count2',
+            'pullup_count3', 'pushup_count3'
+        ]
+        for field in count_fields:
+            # set a hint rather than a filled value
+            self.fields[field].widget.attrs.update({'placeholder': '0'})
+            # clear the initial value for new entries so it's blank
+            if not self.instance.pk and not self.data.get(field):
+                self.fields[field].initial = None
